5746ae957802aa136c9caa253b6228e71261b1cb
Emiliano <emilianoflores180394@gmail.com>
2023-04-18 16:40:48 -0300

    Added background to burger menu (responsive). Approach to add 'success' message in same html (modified script.js). Hid icons overflow when responsive, it happened because of AOS

--- js/scripts.js
+++ js/scripts.js
@@ -160,3 +160,118 @@ function pauseEvent(e){
 
 
 //Before After Slider Function END--<<<
+
+/*Success Message After Form----------------------->>>*/
+/*
+const indexBody = document.getElementsByClassName("fullBody");
+
+const submitButton = document.getElementById ("submitButton")
+submitButton.addEventListener("click", successForm);
+
+function successForm() {
+   document.body.innerHTML= `
+   <!DOCTYPE html>
+   <html lang="en">
+       <head>
+           <meta charset="UTF-8">
+           <meta http-equiv="X-UA-Compatible" content="IE=edge">
+           <meta name="viewport" content="width=device-width, initial-scale=1.0">
+           <title>Enviado!</title>
+           <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,400i,700,900&display=swap" rel="stylesheet">
+           <link   rel="stylesheet"    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+           <link rel="preconnect" href="https://fonts.googleapis.com">
+           <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+           <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,500;1,300&display=swap" rel="stylesheet">
+           <style>
+               body {
+               text-align: center;
+               padding: 40px 0;
+               background: #dcd5e0;
+               }
+               h1 {
+               color: #600293;
+               font-family: 'Plus Jakarta Sans', sans-serif;
+               font-weight: 500;
+               font-size: 40px;
+               margin-bottom: 10px;
+               }
+               p {
+               color: #4f0279;
+               font-family: 'Plus Jakarta Sans', sans-serif;
+               font-weight: 300;
+               font-size:20px;
+               font-style: italic;
+               margin: 0;
+               }
+               i {
+               color: rgb(5, 88, 5);
+               font-size: 100px;
+               line-height: 200px;
+               margin-left:-15px;
+               }
+               .card {
+               background: #d09eeb;
+               padding: 60px;
+               border-radius: 30px;
+               box-shadow: 0 2px 3px #C8D0D8;
+               display: inline-block;
+               margin: 0 auto;
+               }
+               #countdown {
+               font-size: 30px;
+               margin-top: 20px;
+               }    
+               .animate__animated.animate__fadeIn {
+               --animate-delay: 1s;
+               --animate-duration: 10s;
+               }
+               .animate__animated.animate__zoomInDown {
+               --animate-duration: 2s;
+               }    
+               .animate__animated.animate__tada {
+               ---animate-duration: 6s;
+                           }
+               .animate__animated.animate__shakeY {
+               --animate-delay: 1s;
+               --animate-duration: 12s;
+                           }
+           </style>
+       </head>
+       <body>
+           <div class="card animate__animated animate__flipInX">
+           <div class="animate__animated animate__zoomInDown" style="border-radius:200px; height:200px; width:200px; background: white; margin:0 auto;">
+               <i class="checkmark">✓</i>
+           </div>
+           <h1>Enviado!</h1> 
+           <p>Hemos recibido tu consulta, <br/> Nos comunicaremos en breve!</p>
+           
+   
+           <div class="animate__animated animate__shakeY"><img class="animate__animated animate__fadeIn" style="max-height: 80px; padding-bottom: 0px;"src="/assets/img/alineare-logo-2.svg"></div>
+           <div style="color:white; font-size: 15px; font-family:'Plus Jakarta Sans', sans-serif; ;" id="countdown"></div>
+           </div>
+   
+           <script>
+           // Countdown function
+           function countdown() {
+               var seconds = 6;
+               var countdownElement = document.getElementById("countdown");
+   
+               var countdownInterval = setInterval(function() {
+               countdownElement.innerHTML = "Redirigiendo..." ;
+               seconds--;
+               if (seconds < 0) {
+                   clearInterval(countdownInterval);
+                   window.location.href = "../index.html";
+               }
+               }, 1000);
+           }
+   
+           // Start countdown on page load
+            countdown();
+           </script>
+       </body>
+   </html>`
+  
+}*/
+
+/*END Success Message After Form------------------->>>*/ 
